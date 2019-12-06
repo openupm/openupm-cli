9eefb6530610be0309c5536b6c13ee381abca144
Favo Yang <favoyang@gmail.com>
2019-12-06 23:18:33 +0800

    add: unit tests for core

---
+++ test/test-core.js
@@ -0,0 +1,306 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const assert = require("assert");
+const fs = require("fs");
+const nock = require("nock");
+const path = require("path");
+const should = require("should");
+const {
+  env,
+  getCache,
+  parseEnv,
+  parseName,
+  loadManifest,
+  saveManifest,
+  getPackageInfo
+} = require("../lib/core");
+const {
+  getWorkDir,
+  createWorkDir,
+  removeWorkDir,
+  captureStream
+} = require("./utils");
+
+describe("cmd-core.js", function() {
+  describe("parseName", function() {
+    it("pkg@version", function() {
+      parseName("pkg@1.0.0").should.deepEqual({
+        name: "pkg",
+        version: "1.0.0"
+      });
+    });
+    it("pkg@latest", function() {
+      parseName("pkg@latest").should.deepEqual({
+        name: "pkg",
+        version: "latest"
+      });
+    });
+    it("pkg", function() {
+      parseName("pkg").should.deepEqual({
+        name: "pkg",
+        version: undefined
+      });
+    });
+    it("pkg@file", function() {
+      parseName("pkg@file:../pkg").should.deepEqual({
+        name: "pkg",
+        version: "file:../pkg"
+      });
+    });
+    it("pkg@http", function() {
+      parseName("pkg@https://github.com/owner/pkg").should.deepEqual({
+        name: "pkg",
+        version: "https://github.com/owner/pkg"
+      });
+    });
+    it("pkg@git", function() {
+      parseName("pkg@git@github.com:owner/pkg.git").should.deepEqual({
+        name: "pkg",
+        version: "git@github.com:owner/pkg.git"
+      });
+    });
+  });
+
+  describe("parseEnv", function() {
+    let stdout;
+    let stderr;
+    before(function() {
+      removeWorkDir("test-openupm-cli");
+      removeWorkDir("test-openupm-cli-no-manifest");
+      createWorkDir("test-openupm-cli", { withManifest: true });
+      createWorkDir("test-openupm-cli-no-manifest", { withManifest: false });
+    });
+    after(function() {
+      removeWorkDir("test-openupm-cli");
+      removeWorkDir("test-openupm-cli-no-manifest");
+    });
+    beforeEach(function() {
+      stdout = captureStream(process.stdout);
+      stderr = captureStream(process.stderr);
+    });
+    afterEach(function() {
+      stdout.unhook();
+      stderr.unhook();
+    });
+    it("defaults", function() {
+      parseEnv({ parent: {} }, { checkPath: false }).should.be.ok();
+      env.registry.should.equal("https://package.openupm.com");
+      env.namespace.should.equal("com.openupm");
+      env.cwd.should.equal("");
+      env.manifestPath.should.equal("");
+    });
+    it("check path", function() {
+      parseEnv(
+        { parent: { chdir: getWorkDir("test-openupm-cli") } },
+        { checkPath: true }
+      ).should.be.ok();
+      env.cwd.should.be.equal(getWorkDir("test-openupm-cli"));
+      env.manifestPath.should.be.equal(
+        path.join(getWorkDir("test-openupm-cli"), "Packages/manifest.json")
+      );
+    });
+    it("can not resolve path", function() {
+      parseEnv(
+        { parent: { chdir: getWorkDir("path-not-exist") } },
+        { checkPath: true }
+      ).should.not.be.ok();
+      stderr
+        .captured()
+        .includes("can not resolve path")
+        .should.be.ok();
+    });
+    it("can not locate manifest.json", function() {
+      parseEnv(
+        { parent: { chdir: getWorkDir("test-openupm-cli-no-manifest") } },
+        { checkPath: true }
+      ).should.not.be.ok();
+      stderr
+        .captured()
+        .includes("can not locate manifest.json")
+        .should.be.ok();
+    });
+    it("custom registry", function() {
+      parseEnv(
+        { parent: { registry: "https://registry.npmjs.org" } },
+        { checkPath: false }
+      ).should.be.ok();
+      env.registry.should.be.equal("https://registry.npmjs.org");
+      env.namespace.should.be.equal("org.npmjs");
+    });
+    it("custom registry with splash", function() {
+      parseEnv(
+        { parent: { registry: "https://registry.npmjs.org/" } },
+        { checkPath: false }
+      ).should.be.ok();
+      env.registry.should.be.equal("https://registry.npmjs.org");
+      env.namespace.should.be.equal("org.npmjs");
+    });
+    it("custom registry with extra path", function() {
+      parseEnv(
+        { parent: { registry: "https://registry.npmjs.org/some" } },
+        { checkPath: false }
+      ).should.be.ok();
+      env.registry.should.be.equal("https://registry.npmjs.org/some");
+      env.namespace.should.be.equal("org.npmjs");
+    });
+    it("custom registry with extra path and splash", function() {
+      parseEnv(
+        { parent: { registry: "https://registry.npmjs.org/some/" } },
+        { checkPath: false }
+      ).should.be.ok();
+      env.registry.should.be.equal("https://registry.npmjs.org/some");
+      env.namespace.should.be.equal("org.npmjs");
+    });
+    it("custom registry without http", function() {
+      parseEnv(
+        { parent: { registry: "registry.npmjs.org" } },
+        { checkPath: false }
+      ).should.be.ok();
+      env.registry.should.be.equal("http://registry.npmjs.org");
+      env.namespace.should.be.equal("org.npmjs");
+    });
+    it("custom registry with ipv4+port", function() {
+      parseEnv(
+        { parent: { registry: "http://127.0.0.1:4873" } },
+        { checkPath: false }
+      ).should.be.ok();
+      env.registry.should.be.equal("http://127.0.0.1:4873");
+      env.namespace.should.be.equal("127.0.0.1");
+    });
+    it("custom registry with ipv6+port", function() {
+      parseEnv(
+        { parent: { registry: "http://[1:2:3:4:5:6:7:8]:4873" } },
+        { checkPath: false }
+      ).should.be.ok();
+      env.registry.should.be.equal("http://[1:2:3:4:5:6:7:8]:4873");
+      env.namespace.should.be.equal("1:2:3:4:5:6:7:8");
+    });
+  });
+
+  describe("loadManifest/SaveManifest", function() {
+    let stdout;
+    let stderr;
+    beforeEach(function() {
+      removeWorkDir("test-openupm-cli");
+      createWorkDir("test-openupm-cli", { withManifest: true });
+      createWorkDir("test-openupm-cli-wrong-json", {
+        withManifest: true
+      });
+      fs.writeFileSync(
+        path.join(
+          getWorkDir("test-openupm-cli-wrong-json"),
+          "Packages/manifest.json"
+        ),
+        "wrong-json"
+      );
+      stdout = captureStream(process.stdout);
+      stderr = captureStream(process.stderr);
+    });
+    afterEach(function() {
+      removeWorkDir("test-openupm-cli");
+      removeWorkDir("test-openupm-cli-wrong-json");
+      stdout.unhook();
+      stderr.unhook();
+    });
+    it("loadManifest", function() {
+      parseEnv(
+        { parent: { chdir: getWorkDir("test-openupm-cli") } },
+        { checkPath: true }
+      ).should.be.ok();
+      const manifest = loadManifest();
+      manifest.should.be.deepEqual({ dependencies: {} });
+    });
+    it("no manifest file", function() {
+      parseEnv(
+        { parent: { chdir: getWorkDir("path-not-exist") } },
+        { checkPath: false }
+      ).should.be.ok();
+      const manifest = loadManifest();
+      (manifest === null).should.be.ok();
+      stderr
+        .captured()
+        .includes("does not exist")
+        .should.be.ok();
+    });
+    it("wrong json content", function() {
+      parseEnv(
+        { parent: { chdir: getWorkDir("test-openupm-cli-wrong-json") } },
+        { checkPath: true }
+      ).should.be.ok();
+      const manifest = loadManifest();
+      (manifest === null).should.be.ok();
+      stderr
+        .captured()
+        .includes("failed to parse")
+        .should.be.ok();
+    });
+    it("saveManifest", function() {
+      parseEnv(
+        { parent: { chdir: getWorkDir("test-openupm-cli") } },
+        { checkPath: true }
+      ).should.be.ok();
+      const manifest = loadManifest();
+      manifest.should.be.deepEqual({ dependencies: {} });
+      manifest.dependencies["some-pack"] = "1.0.0";
+      saveManifest(manifest).should.be.ok();
+      const manifest2 = loadManifest();
+      manifest2.should.be.deepEqual(manifest);
+    });
+  });
+
+  describe("getCache", function() {
+    it("simple", function() {
+      const cache = getCache();
+      (cache === null).should.be.false();
+      (cache === undefined).should.be.false();
+    });
+  });
+
+  describe("getPackageInfo", function() {
+    let stdout;
+    let stderr;
+    let nockScope;
+    beforeEach(function() {
+      stdout = captureStream(process.stdout);
+      stderr = captureStream(process.stderr);
+    });
+    afterEach(function() {
+      if (nockScope) {
+        nockScope.done();
+        nockScope = null;
+      }
+      nock.cleanAll();
+      nock.enableNetConnect();
+      stdout = captureStream(process.stdout);
+      stderr = captureStream(process.stderr);
+    });
+    it("simple", async function() {
+      parseEnv(
+        { parent: { registry: "http://example.com" } },
+        { checkPath: false }
+      ).should.be.ok();
+      let pkgInfoRemote = { name: "com.littlebigfun.addressable-importer" };
+      nockScope = nock("http://example.com")
+        .get("/package-a")
+        .reply(200, pkgInfoRemote, { "Content-Type": "application/json" });
+      const info = await getPackageInfo("package-a");
+      info.should.deepEqual(pkgInfoRemote);
+    });
+    it("404", async function() {
+      parseEnv(
+        { parent: { registry: "http://example.com" } },
+        { checkPath: false }
+      ).should.be.ok();
+      let pkgInfoRemote = { name: "com.littlebigfun.addressable-importer" };
+      nockScope = nock("http://example.com")
+        .get("/package-a")
+        .reply(404);
+      const info = await getPackageInfo("package-a");
+      (info === undefined).should.be.ok();
+      stderr
+        .captured()
+        .includes("package not found")
+        .should.be.ok();
+    });
+  });
+});
---
+++ test/utils.js
@@ -0,0 +1,42 @@
+const fse = require("fs-extra");
+const path = require("path");
+const os = require("os");
+
+const getWorkDir = function(pathToTmp) {
+  return path.join(os.tmpdir(), pathToTmp);
+};
+
+const createWorkDir = function(pathToTmp, { withManifest }) {
+  const workDir = getWorkDir(pathToTmp);
+  fse.mkdirpSync(workDir);
+  if (withManifest) {
+    const manifestDir = path.join(workDir, "Packages");
+    fse.mkdirpSync(manifestDir);
+    const data = JSON.stringify({ dependencies: {} });
+    fse.writeFileSync(path.join(manifestDir, "manifest.json"), data);
+  }
+};
+
+const removeWorkDir = function(pathToTmp) {
+  const cwd = getWorkDir(pathToTmp);
+  fse.removeSync(cwd);
+};
+
+function captureStream(stream) {
+  var oldWrite = stream.write;
+  var buf = "";
+  stream.write = function(chunk, encoding, callback) {
+    buf += chunk.toString(); // chunk is a String or Buffer
+    oldWrite.apply(stream, arguments);
+  };
+
+  return {
+    unhook: function unhook() {
+      stream.write = oldWrite;
+    },
+    captured: function() {
+      return buf;
+    }
+  };
+}
+module.exports = { getWorkDir, createWorkDir, removeWorkDir, captureStream };
