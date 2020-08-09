b41725f2ed82f3f73bf43f44d763c3c564aca095
Favo Yang <favoyang@gmail.com>
2020-08-09 12:01:03 +0800

    feat: support auth for add, deps, remove, and view commands

--- test/test-core.js
+++ test/test-core.js
@@ -85,8 +85,8 @@ describe("cmd-core.js", function() {
       stdoutInspect.restore();
       stderrInspect.restore();
     });
-    it("defaults", function() {
-      parseEnv({ parent: {} }, { checkPath: false }).should.be.ok();
+    it("defaults", async function() {
+      (await parseEnv({ parent: {} }, { checkPath: false })).should.be.ok();
       env.registry.should.equal("https://package.openupm.com");
       env.upstream.should.be.ok();
       env.upstreamRegistry.should.equal("https://packages.unity.com");
@@ -95,10 +95,12 @@ describe("cmd-core.js", function() {
       env.manifestPath.should.equal("");
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("check path", function() {
-      parseEnv(
-        { parent: { chdir: getWorkDir("test-openupm-cli") } },
-        { checkPath: true }
+    it("check path", async function() {
+      (
+        await parseEnv(
+          { parent: { chdir: getWorkDir("test-openupm-cli") } },
+          { checkPath: true }
+        )
       ).should.be.ok();
       env.cwd.should.be.equal(getWorkDir("test-openupm-cli"));
       env.manifestPath.should.be.equal(
@@ -106,89 +108,106 @@ describe("cmd-core.js", function() {
       );
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("can not resolve path", function() {
-      parseEnv(
-        { parent: { chdir: getWorkDir("path-not-exist") } },
-        { checkPath: true }
+    it("can not resolve path", async function() {
+      (
+        await parseEnv(
+          { parent: { chdir: getWorkDir("path-not-exist") } },
+          { checkPath: true }
+        )
       ).should.not.be.ok();
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
       stdout.includes("can not resolve path").should.be.ok();
     });
-    it("can not locate manifest.json", function() {
-      parseEnv(
-        { parent: { chdir: getWorkDir("test-openupm-cli-no-manifest") } },
-        { checkPath: true }
+    it("can not locate manifest.json", async function() {
+      (
+        await parseEnv(
+          { parent: { chdir: getWorkDir("test-openupm-cli-no-manifest") } },
+          { checkPath: true }
+        )
       ).should.not.be.ok();
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
       stdout.includes("can not locate manifest.json").should.be.ok();
     });
-    it("custom registry", function() {
-      parseEnv(
-        { parent: { registry: "https://registry.npmjs.org" } },
-        { checkPath: false }
+    it("custom registry", async function() {
+      (
+        await parseEnv(
+          { parent: { registry: "https://registry.npmjs.org" } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       env.registry.should.be.equal("https://registry.npmjs.org");
       env.namespace.should.be.equal("org.npmjs");
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("custom registry with splash", function() {
-      parseEnv(
-        { parent: { registry: "https://registry.npmjs.org/" } },
-        { checkPath: false }
+    it("custom registry with splash", async function() {
+      (
+        await parseEnv(
+          { parent: { registry: "https://registry.npmjs.org/" } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       env.registry.should.be.equal("https://registry.npmjs.org");
       env.namespace.should.be.equal("org.npmjs");
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("custom registry with extra path", function() {
-      parseEnv(
-        { parent: { registry: "https://registry.npmjs.org/some" } },
-        { checkPath: false }
+    it("custom registry with extra path", async function() {
+      (
+        await parseEnv(
+          { parent: { registry: "https://registry.npmjs.org/some" } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       env.registry.should.be.equal("https://registry.npmjs.org/some");
       env.namespace.should.be.equal("org.npmjs");
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("custom registry with extra path and splash", function() {
-      parseEnv(
-        { parent: { registry: "https://registry.npmjs.org/some/" } },
-        { checkPath: false }
+    it("custom registry with extra path and splash", async function() {
+      (
+        await parseEnv(
+          { parent: { registry: "https://registry.npmjs.org/some/" } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       env.registry.should.be.equal("https://registry.npmjs.org/some");
       env.namespace.should.be.equal("org.npmjs");
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("custom registry without http", function() {
-      parseEnv(
-        { parent: { registry: "registry.npmjs.org" } },
-        { checkPath: false }
+    it("custom registry without http", async function() {
+      (
+        await parseEnv(
+          { parent: { registry: "registry.npmjs.org" } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       env.registry.should.be.equal("http://registry.npmjs.org");
       env.namespace.should.be.equal("org.npmjs");
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("custom registry with ipv4+port", function() {
-      parseEnv(
-        { parent: { registry: "http://127.0.0.1:4873" } },
-        { checkPath: false }
+    it("custom registry with ipv4+port", async function() {
+      (
+        await parseEnv(
+          { parent: { registry: "http://127.0.0.1:4873" } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       env.registry.should.be.equal("http://127.0.0.1:4873");
       env.namespace.should.be.equal("127.0.0.1");
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("custom registry with ipv6+port", function() {
-      parseEnv(
-        { parent: { registry: "http://[1:2:3:4:5:6:7:8]:4873" } },
-        { checkPath: false }
+    it("custom registry with ipv6+port", async function() {
+      (
+        await parseEnv(
+          { parent: { registry: "http://[1:2:3:4:5:6:7:8]:4873" } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       env.registry.should.be.equal("http://[1:2:3:4:5:6:7:8]:4873");
       env.namespace.should.be.equal("1:2:3:4:5:6:7:8");
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("upstream", function() {
-      parseEnv(
-        { parent: { upstream: false } },
-        { checkPath: false }
+    it("upstream", async function() {
+      (
+        await parseEnv({ parent: { upstream: false } }, { checkPath: false })
       ).should.be.ok();
       env.upstream.should.not.be.ok();
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
@@ -219,39 +238,47 @@ describe("cmd-core.js", function() {
       stdoutInspect.restore();
       stderrInspect.restore();
     });
-    it("loadManifest", function() {
-      parseEnv(
-        { parent: { chdir: getWorkDir("test-openupm-cli") } },
-        { checkPath: true }
+    it("loadManifest", async function() {
+      (
+        await parseEnv(
+          { parent: { chdir: getWorkDir("test-openupm-cli") } },
+          { checkPath: true }
+        )
       ).should.be.ok();
       const manifest = loadManifest();
       manifest.should.be.deepEqual({ dependencies: {} });
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
     });
-    it("no manifest file", function() {
-      parseEnv(
-        { parent: { chdir: getWorkDir("path-not-exist") } },
-        { checkPath: false }
+    it("no manifest file", async function() {
+      (
+        await parseEnv(
+          { parent: { chdir: getWorkDir("path-not-exist") } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       const manifest = loadManifest();
       (manifest === null).should.be.ok();
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
       stdout.includes("does not exist").should.be.ok();
     });
-    it("wrong json content", function() {
-      parseEnv(
-        { parent: { chdir: getWorkDir("test-openupm-cli-wrong-json") } },
-        { checkPath: true }
+    it("wrong json content", async function() {
+      (
+        await parseEnv(
+          { parent: { chdir: getWorkDir("test-openupm-cli-wrong-json") } },
+          { checkPath: true }
+        )
       ).should.be.ok();
       const manifest = loadManifest();
       (manifest === null).should.be.ok();
       const [stdout, stderr] = getOutputs(stdoutInspect, stderrInspect);
       stdout.includes("failed to parse").should.be.ok();
     });
-    it("saveManifest", function() {
-      parseEnv(
-        { parent: { chdir: getWorkDir("test-openupm-cli") } },
-        { checkPath: true }
+    it("saveManifest", async function() {
+      (
+        await parseEnv(
+          { parent: { chdir: getWorkDir("test-openupm-cli") } },
+          { checkPath: true }
+        )
       ).should.be.ok();
       const manifest = loadManifest();
       manifest.should.be.deepEqual({ dependencies: {} });
@@ -279,9 +306,11 @@ describe("cmd-core.js", function() {
       nockDown();
     });
     it("simple", async function() {
-      parseEnv(
-        { parent: { registry: "http://example.com" } },
-        { checkPath: false }
+      (
+        await parseEnv(
+          { parent: { registry: "http://example.com" } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       let pkgInfoRemote = { name: "com.littlebigfun.addressable-importer" };
       nock("http://example.com")
@@ -291,9 +320,11 @@ describe("cmd-core.js", function() {
       info.should.deepEqual(pkgInfoRemote);
     });
     it("404", async function() {
-      parseEnv(
-        { parent: { registry: "http://example.com" } },
-        { checkPath: false }
+      (
+        await parseEnv(
+          { parent: { registry: "http://example.com" } },
+          { checkPath: false }
+        )
       ).should.be.ok();
       let pkgInfoRemote = { name: "com.littlebigfun.addressable-importer" };
       nock("http://example.com")
