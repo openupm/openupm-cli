309a8e0e4a4e61ef595a4f725f88280a4fa255e5
ComradeVanti <kingvanti650@gmail.com>
2023-10-18 19:18:06 +0200

    chore: fix eslint end-of-line error
    On my Windows machine eslint was constantly complaining about line endings. Fixed using this answer https://stackoverflow.com/a/53769213

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,12 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
