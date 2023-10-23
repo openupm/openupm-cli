09846fc3d1bee9de4447ef6bf26eaf29bbb471e0
ComradeVanti <kingvanti650@gmail.com>
2023-10-23 21:45:10 +0200

    fix: remove extraneous declare
    It seems to cause problems and to be honest I don't know if we need it

--- lib/types/npm-registry-fetch.d.ts
+++ lib/types/npm-registry-fetch.d.ts
@@ -1,7 +1,7 @@
 import { Response } from "request";
 
 declare module "npm-registry-fetch" {
-  declare class HttpErrorBase extends Error {
+  class HttpErrorBase extends Error {
     statusCode: Response["status"];
     code: `E${Response["status"]}}` | `E${string}`;
   }
