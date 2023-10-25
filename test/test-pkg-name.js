49fafd096c69f34c7dc16b17029e0ff1525c3c51
ComradeVanti <kingvanti650@gmail.com>
2023-10-25 11:08:10 +0200

    refactor: extract and rename function
    Rename the parseName function to splitPackageName to better reflect it's purpose. Also moved to separate module

---
+++ test/test-pkg-name.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const assert = require("assert");
+const fs = require("fs");
+const nock = require("nock");
+const path = require("path");
+const should = require("should");
+const { splitPkgName } = require("../build/lib/utils/pkg-name");
+describe("pkg-name.ts", function () {
+  describe("splitPkgName", function () {
+    it("pkg@version", function () {
+      splitPkgName("pkg@1.0.0").should.deepEqual({
+        name: "pkg",
+        version: "1.0.0",
+      });
+    });
+    it("pkg@latest", function () {
+      splitPkgName("pkg@latest").should.deepEqual({
+        name: "pkg",
+        version: "latest",
+      });
+    });
+    it("pkg", function () {
+      splitPkgName("pkg").should.deepEqual({
+        name: "pkg",
+        version: undefined,
+      });
+    });
+    it("pkg@file", function () {
+      splitPkgName("pkg@file:../pkg").should.deepEqual({
+        name: "pkg",
+        version: "file:../pkg",
+      });
+    });
+    it("pkg@http", function () {
+      splitPkgName("pkg@https://github.com/owner/pkg").should.deepEqual({
+        name: "pkg",
+        version: "https://github.com/owner/pkg",
+      });
+    });
+    it("pkg@git", function () {
+      splitPkgName("pkg@git@github.com:owner/pkg.git").should.deepEqual({
+        name: "pkg",
+        version: "git@github.com:owner/pkg.git",
+      });
+    });
+  });
+});
