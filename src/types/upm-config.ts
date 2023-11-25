a0181768fd900f6cb2bdf7d262d5f7871d4f97a6
Ramon Brullo <kingvanti650@gmail.com>
2023-11-25 12:20:10 +0100

    doc: jsdoc for upm-config types

--- src/types/upm-config.ts
+++ src/types/upm-config.ts
@@ -1,10 +1,39 @@
 import { Registry } from "./global";
 
+/**
+ * Authentication information for a registry
+ */
 export type UpmAuth = {
+  /**
+   * The email to use
+   */
   email: string;
+  /**
+   * Whether to always authenticate
+   */
   alwaysAuth: boolean;
-} & ({ token: string } | { _auth: string });
+} & (
+  | {
+      /**
+       * A token to authenticate with
+       */
+      token: string;
+    }
+  | {
+      /**
+       * Base64 encoded username and password to authenticate with
+       */
+      _auth: string;
+    }
+);
 
+/**
+ * Content of .upmconfig.toml. Used to authenticate with package registries
+ * @see https://medium.com/openupm/how-to-authenticate-with-a-upm-scoped-registry-using-cli-afc29c13a2f8
+ */
 export type UPMConfig = {
+  /**
+   * Authentication information organized by the registry they should be used on
+   */
   npmAuth?: Record<Registry, UpmAuth>;
 };
