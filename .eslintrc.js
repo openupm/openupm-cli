6bcbac1b0f0a3ba78b45ce7610b69033a7a15c3d
Ramon Brullo <kingvanti650@gmail.com>
2024-01-13 14:47:01 +0100

    conf: enforce consistent function style

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "func-style": ["error", "declaration", { allowArrowFunctions: true }],
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
