d86412c79f4d0ed38f123ada6f6830df853c9acd
Ramon Brullo <kingvanti650@gmail.com>
2024-01-14 10:13:48 +0100

    conf: add jsdoc lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,17 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:jsdoc/recommended-typescript",
     "prettier",
   ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "jsdoc/require-returns": 0,
+    "jsdoc/require-jsdoc": 0,
+    "jsdoc/require-param": 0,
     "jsdoc/require-throws": 1,
+    "jsdoc/check-param-names": [1, { checkDestructured: false }],
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "jsdoc"],
