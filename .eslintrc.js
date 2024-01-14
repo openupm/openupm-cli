e8b2346a712abf548719674446fcb67e82197abe
Ramon Brullo <kingvanti650@gmail.com>
2024-01-14 10:09:09 +0100

    conf: require throws in jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "jsdoc/require-throws": 1,
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "jsdoc"],
