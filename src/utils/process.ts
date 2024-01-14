1061b73bc9ac9b1413db384f003b269775c1f9ba
Ramon Brullo <kingvanti650@gmail.com>
2024-01-14 10:51:30 +0100

    docs: document parameter in jsDoc

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -2,7 +2,8 @@ import childProcess from "child_process";
 
 /**
  * @param command A shell command to execute.
- * @returns A promise that resolve to the output of the shell command, or an 
+ * @param trim Whether to trim whitespace from command output.
+ * @returns A promise that resolve to the output of the shell command, or an
  * error.
  * @example const output = await execute("ls -alh");
  */
@@ -17,10 +18,10 @@ export default function execute(
    */
   return new Promise(function (resolve, reject) {
     /**
-     * @param {Error} error An error triggered during the execution of the 
+     * @param {Error} error An error triggered during the execution of the
      * childProcess.exec command.
      * @param {string|Buffer} stdout The result of the shell command execution.
-     * @param {string|Buffer} stderr The error resulting of the shell command 
+     * @param {string|Buffer} stderr The error resulting of the shell command
      * execution.
      * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
      */
