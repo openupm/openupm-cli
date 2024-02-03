08b9b2c4e9e702e9d8cfd25db8c53d068c7aec1e
Ramon Brullo <kingvanti650@gmail.com>
2024-02-03 16:08:14 +0100

    docs: add jsdoc to cache code

--- src/packument-cache.ts
+++ src/packument-cache.ts
@@ -1,10 +1,30 @@
 import { DomainName } from "./types/domain-name";
 import { UnityPackument } from "./types/packument";
 
-type CachedPackument = { packument: UnityPackument; upstream: boolean };
+/**
+ * Represents a cached packument.
+ */
+type CachedPackument = {
+  /**
+   * The packument.
+   */
+  packument: UnityPackument;
+  /**
+   * Whether it was originally resolved from the upstream registry.
+   */
+  upstream: boolean;
+};
 
+/**
+ * A cache that contains packuments that were already resolved.
+ */
 export type PackumentCache = Record<DomainName, CachedPackument>;
 
+/**
+ * Attempts to get a cached packument from a cache.
+ * @param packageName The name of the packument.
+ * @param cache The cache.
+ */
 export function tryGetFromCache(
   packageName: DomainName,
   cache: PackumentCache
@@ -12,6 +32,14 @@ export function tryGetFromCache(
   return cache[packageName] ?? null;
 }
 
+/**
+ * Caches a packument.
+ * @param packageName The packuments name.
+ * @param packument The packument.
+ * @param upstream Whether the packument was resolved from the upstream registry.
+ * @param cache The current state of the cache.
+ * @returns The new state of the cache.
+ */
 export function addToCache(
   packageName: DomainName,
   packument: UnityPackument,
