2407b106c820db45841d7f71153d4788228a39a9
Ramon Brullo <kingvanti650@gmail.com>
2024-03-18 11:50:51 +0100

    refactor: remove unused code

--- src/types/domain-name.ts
+++ src/types/domain-name.ts
@@ -15,36 +15,6 @@ function domainSegmentsIn(hostName: string): string[] {
 }
 
 /**
- * Creates a namespace by reversing the TDL of a host-name.
- * @param hostname The host-name to reverse.
- * @example unity.com becomes com.unity.
- * @example registry.npmjs.org becomes org.npmjs.
- * @example my-school.ac.at becomes at.ac.my-school
- */
-export function namespaceFor(hostname: string): DomainName {
-  const segments = domainSegmentsIn(hostname);
-  const namespaceSegments = (function () {
-    const count = segments.length;
-
-    /* 
-    NOTE: This function does not handle domains with longer extensions
-    such as registry.example.team.com. In this case it would incorrectly only
-    return "com.team" even though "example" is also part of the domain.
-    Let's just hope this does not happen for now 🤞
-    */
-
-    // 2-part domains, like unity.com
-    if (count < 3) return segments;
-    // Domains with two short extensions like my-school.ac.at
-    if (segments[count - 1]!.length <= 3 && segments[count - 2]!.length <= 3)
-      return segments.slice(count - 3);
-    // Domains with one extension such as registry.npmjs.org
-    return segments.slice(count - 2);
-  })();
-  return namespaceSegments.reverse().join(".") as DomainName;
-}
-
-/**
  * Checks if a string is a domain name. Only does basic syntax validation.
  * Does not check for correct segment-count etc.
  * @param s The string.
