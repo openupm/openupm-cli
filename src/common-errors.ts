5344a7508e664a8eb6ac9726215d3354e4207033
Ramon Brullo <kingvanti650@gmail.com>
2024-03-18 19:16:34 +0100

    docs: add property jsdoc

--- src/common-errors.ts
+++ src/common-errors.ts
@@ -5,7 +5,12 @@ import { CustomError } from "ts-custom-error";
  * was not found.
  */
 export class RequiredFileNotFoundError extends CustomError {
-  constructor(readonly path: string) {
+  constructor(
+    /**
+     * The path where the file was expected to be.
+     */
+    readonly path: string
+  ) {
     super(`The required file at "${path}" could not be found.`);
   }
 }
