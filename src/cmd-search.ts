51e93365fe586703f62f4fca4263ca9fcd17ae2a
Ramon Brullo <kingvanti650@gmail.com>
2024-03-19 09:12:45 +0100

    refactor: use async result (#181)
    * refactor: use async-result type
    
    * refactor: use async-result type
    
    * refactor: use async-result type

--- src/cmd-search.ts
+++ src/cmd-search.ts
@@ -9,31 +9,30 @@ import {
   SearchedPackument,
 } from "./npm-client";
 import { formatAsTable } from "./output-formatting";
-import { Ok, Result } from "ts-results-es";
+import { AsyncResult, Ok, Result } from "ts-results-es";
 import { HttpErrorBase } from "npm-registry-fetch";
 
 export type SearchError = EnvParseError | HttpErrorBase;
 
 export type SearchOptions = CmdOptions;
 
-const searchEndpoint = async function (
+const searchEndpoint = function (
   npmClient: NpmClient,
   registry: Registry,
   keyword: string
-): Promise<Result<SearchedPackument[], HttpErrorBase>> {
-  const results = await npmClient.trySearch(registry, keyword).promise;
-
-  if (results.isOk()) log.verbose("npmsearch", results.value.join(os.EOL));
-
-  return results;
+): AsyncResult<SearchedPackument[], HttpErrorBase> {
+  return npmClient.trySearch(registry, keyword).map((results) => {
+    log.verbose("npmsearch", results.join(os.EOL));
+    return results;
+  });
 };
 
-const searchOld = async function (
+const searchOld = function (
   npmClient: NpmClient,
   registry: Registry,
   keyword: string
-): Promise<Result<SearchedPackument[], HttpErrorBase>> {
-  return (await npmClient.tryGetAll(registry).promise).map((allPackuments) => {
+): AsyncResult<SearchedPackument[], HttpErrorBase> {
+  return npmClient.tryGetAll(registry).map((allPackuments) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { _updated, ...packumentEntries } = allPackuments;
     const packuments = Object.values(packumentEntries);
@@ -61,12 +60,12 @@ export async function search(
   const npmClient = makeNpmClient();
 
   // search endpoint
-  let result = await searchEndpoint(npmClient, env.registry, keyword);
+  let result = await searchEndpoint(npmClient, env.registry, keyword).promise;
 
   // search old search
   if (result.isErr()) {
     log.warn("", "fast search endpoint is not available, using old search.");
-    result = await searchOld(npmClient, env.registry, keyword);
+    result = await searchOld(npmClient, env.registry, keyword).promise;
   }
   if (result.isErr()) {
     log.warn("", "/-/all endpoint is not available");
