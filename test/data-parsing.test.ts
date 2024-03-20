aa5b957e47050e3921a1c9c36ed21b442b39c24c
Ramon Brullo <kingvanti650@gmail.com>
2024-03-20 10:02:46 +0100

    refactor: result json parse (#188)
    * feat: add json-parse function
    
    * refactor: use json-parse function

---
+++ test/data-parsing.test.ts
@@ -0,0 +1,24 @@
+import { tryParseJson } from "../src/utils/data-parsing";
+
+describe("data-parsing", () => {
+  describe("json", () => {
+    it("should round-trip valid json", () => {
+      const expected = { test: 123 };
+      const json = JSON.stringify(expected);
+
+      const result = tryParseJson(json);
+
+      expect(result).toBeOk((actual) => expect(actual).toEqual(expected));
+    });
+
+    it("should notify of parse error", () => {
+      const json = "{ invalid json ";
+
+      const result = tryParseJson(json);
+
+      expect(result).toBeError((error) =>
+        expect(error).toBeInstanceOf(SyntaxError)
+      );
+    });
+  });
+});
