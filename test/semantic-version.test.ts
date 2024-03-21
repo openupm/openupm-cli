27b4188b4ef00abe6d9e9ec6635b32edd7b692ce
Ramon Brullo <kingvanti650@gmail.com>
2024-03-21 16:40:38 +0100

    refactor: use it.each

--- test/semantic-version.test.ts
+++ test/semantic-version.test.ts
@@ -2,11 +2,10 @@ import { isSemanticVersion } from "../src/types/semantic-version";
 
 describe("semantic-version", () => {
   describe("validate", () => {
-    ["1.2.3", "1.2.3-alpha"].forEach((input) =>
-      it(`"${input}" is a semantic version`, function () {
-        expect(isSemanticVersion(input)).toBeTruthy();
-      })
-    );
+    it.each(["1.2.3", "1.2.3-alpha"])(`should be ok for "%s"`, (input) => {
+      expect(isSemanticVersion(input)).toBeTruthy();
+    });
+
     ["", " ", "wow", "1", "1.2"].forEach((input) =>
       it(`"${input}" is not a semantic version`, function () {
         expect(isSemanticVersion(input)).not.toBeTruthy();
