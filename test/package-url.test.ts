05d862ac8275383fab0e53614634628bd32d548f
Ramon Brullo <kingvanti650@gmail.com>
2024-03-21 16:36:43 +0100

    refactor: use it.each

--- test/package-url.test.ts
+++ test/package-url.test.ts
@@ -2,15 +2,13 @@ import { isPackageUrl } from "../src/types/package-url";
 
 describe("package-url", () => {
   describe("validation", () => {
-    [
+    it.each([
       "https://github.com/yo/com.base.package-a",
       "git@github.com:yo/com.base.package-a",
       "file../yo/com.base.package-a",
-    ].forEach((url) =>
-      it(`"${url}" is a package-url`, function () {
-        expect(isPackageUrl(url)).toBeTruthy();
-      })
-    );
+    ])(`should be ok for "%s"`, (url) => {
+      expect(isPackageUrl(url)).toBeTruthy();
+    });
 
     ["", "com.base.package.a"].forEach((url) =>
       it(`"${url}" is not a package-url`, function () {
