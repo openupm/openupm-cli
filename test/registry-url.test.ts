f771f27b47138577859e3d1660a331edd0edf166
Ramon Brullo <kingvanti650@gmail.com>
2024-03-21 16:39:33 +0100

    refactor: use it.each

--- test/registry-url.test.ts
+++ test/registry-url.test.ts
@@ -8,21 +8,22 @@ describe("registry-url", () => {
         expect(isRegistryUrl(input)).toBeTruthy();
       }
     );
-    [
+
+    it.each([
       // Missing protocol
       "registry.npmjs.org/",
       // Trailing slash
       "http://registry.npmjs.org/",
-    ].forEach((input) =>
-      it(`"${input}" should not be registry-url`, function () {
-        expect(isRegistryUrl(input)).not.toBeTruthy();
-      })
-    );
+    ])(`"should not be ok for "%s"`, (input) => {
+      expect(isRegistryUrl(input)).not.toBeTruthy();
+    });
   });
+  
   describe("coerce", () => {
     it("should coerce urls without protocol", () => {
       expect(coerceRegistryUrl("test.com")).toEqual("http://test.com");
     });
+    
     it("should remove trailing slash", () => {
       expect(coerceRegistryUrl("http://test.com/")).toEqual("http://test.com");
     });
