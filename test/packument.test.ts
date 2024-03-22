69139aa16b8e3a71157e1fe40c98e872f6e3f792
Ramon Brullo <kingvanti650@gmail.com>
2024-03-22 10:37:21 +0100

    refactor: extract helper function
    Extract logic for getting the editor-version string, e.g. "2020.3.1f1", from a packument-version, into it's own function.

--- test/packument.test.ts
+++ test/packument.test.ts
@@ -1,5 +1,11 @@
-import { tryGetLatestVersion } from "../src/types/packument";
+import {
+  targetEditorVersionFor,
+  tryGetLatestVersion,
+  UnityPackumentVersion,
+} from "../src/types/packument";
 import { makeSemanticVersion } from "../src/types/semantic-version";
+import fc from "fast-check";
+import { arbDomainName } from "./domain-name.arb";
 
 describe("packument", () => {
   describe("tryGetLatestVersion", () => {
@@ -8,6 +14,57 @@ describe("packument", () => {
         "dist-tags": { latest: makeSemanticVersion("1.0.0") },
       });
       expect(version).toEqual("1.0.0");
+    });
+  });
+
+  describe("determine editor-version", () => {
+    it("should get version without release", () => {
+      fc.assert(
+        fc.property(arbDomainName, (packumentName) => {
+          const expected = "2020.3";
+          const packumentVersion: UnityPackumentVersion = {
+            name: packumentName,
+            version: makeSemanticVersion("1.0.0"),
+            unity: expected,
+          };
+
+          const actual = targetEditorVersionFor(packumentVersion);
+
+          expect(actual).toEqual(expected);
+        })
+      );
+    });
+
+    it("should get version with release", () => {
+      fc.assert(
+        fc.property(arbDomainName, (packumentName) => {
+          const packumentVersion: UnityPackumentVersion = {
+            name: packumentName,
+            version: makeSemanticVersion("1.0.0"),
+            unity: "2020.3",
+            unityRelease: "1",
+          };
+
+          const actual = targetEditorVersionFor(packumentVersion);
+
+          expect(actual).toEqual("2020.3.1");
+        })
+      );
+    });
+
+    it("should be null for missing major.minor", () => {
+      fc.assert(
+        fc.property(arbDomainName, (packumentName) => {
+          const packumentVersion: UnityPackumentVersion = {
+            name: packumentName,
+            version: makeSemanticVersion("1.0.0"),
+          };
+
+          const actual = targetEditorVersionFor(packumentVersion);
+
+          expect(actual).toBeNull();
+        })
+      );
     });
   });
 });
