400b47aca1c1e7885ea87fecb28e1935b3fc9685
Ramon Brullo <kingvanti650@gmail.com>
2024-03-22 09:38:15 +0100

    test: improve package-url validation tests
    Use prop tests for validation.

--- test/package-url.test.ts
+++ test/package-url.test.ts
@@ -1,13 +1,43 @@
 import { isPackageUrl } from "../src/types/package-url";
+import fc from "fast-check";
+import { arbDomainName } from "./domain-name.gen";
 
 describe("package-url", () => {
   describe("validation", () => {
-    it.each([
-      "https://github.com/yo/com.base.package-a",
-      "git@github.com:yo/com.base.package-a",
-      "file../yo/com.base.package-a",
-    ])(`should be ok for "%s"`, (url) => {
-      expect(isPackageUrl(url)).toBeTruthy();
+    it("should be ok for http", () => {
+      fc.assert(
+        fc.property(arbDomainName, (packumentName) => {
+          const input = `http://github.com/user/${{ packumentName }}`;
+          expect(isPackageUrl(input)).toBeTruthy();
+        })
+      );
+    });
+
+    it("should be ok for https", () => {
+      fc.assert(
+        fc.property(arbDomainName, (packumentName) => {
+          const input = `https://github.com/user/${{ packumentName }}`;
+          expect(isPackageUrl(input)).toBeTruthy();
+        })
+      );
+    });
+
+    it("should be ok for git", () => {
+      fc.assert(
+        fc.property(arbDomainName, (packumentName) => {
+          const input = `git@github:user/${{ packumentName }}`;
+          expect(isPackageUrl(input)).toBeTruthy();
+        })
+      );
+    });
+
+    it("should be ok for file", () => {
+      fc.assert(
+        fc.property(arbDomainName, (packumentName) => {
+          const input = `file:/path/to/${{ packumentName }}`;
+          expect(isPackageUrl(input)).toBeTruthy();
+        })
+      );
     });
 
     it.each(["", "com.base.package.a"])(`should not be ok for "%s"`, (url) => {
