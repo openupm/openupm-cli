dda2b3a457101701dd25f123af8afac49c8ecd61
Ramon Brullo <kingvanti650@gmail.com>
2024-03-23 19:03:39 +0100

    refactor: improve type declaration

--- src/types/another-npm-registry-client.d.ts
+++ src/types/another-npm-registry-client.d.ts
@@ -1,49 +1,49 @@
 declare module "another-npm-registry-client" {
   import { type Response } from "request";
 
-  namespace RegClient {
-    type NpmAuth =
-      | {
-          username: string;
-          password: string;
-          email: string;
-          alwaysAuth?: boolean;
-        }
-      | { token: string; alwaysAuth?: boolean };
+  type NpmAuth =
+    | {
+        username: string;
+        password: string;
+        email: string;
+        alwaysAuth?: boolean;
+      }
+    | { token: string; alwaysAuth?: boolean };
 
-    type AddUserParams = { auth: NpmAuth };
+  type AddUserParams = { auth: NpmAuth };
 
-    type GetParams = {
-      timeout?: number;
-      follow?: boolean;
-      staleOk?: boolean;
-      auth?: NpmAuth;
-      fullMetadata?: boolean;
-    };
+  type GetParams = {
+    timeout?: number;
+    follow?: boolean;
+    staleOk?: boolean;
+    auth?: NpmAuth;
+    fullMetadata?: boolean;
+  };
 
-    type AddUserResponse = { ok: true; token: string } | { ok: false };
+  type AddUserResponse = { ok: true; token: string } | { ok: false };
 
-    type ClientCallback<TData> = (
-      error: Error | null,
-      data: TData,
-      raw: string,
-      res: Response
-    ) => void;
+  type ClientCallback<TData> = (
+    error: Error | null,
+    data: TData,
+    raw: string,
+    res: Response
+  ) => void;
 
-    type Instance = {
-      get(
-        uri: string,
-        params: GetParams,
-        cb: ClientCallback<import("../domain/packument").UnityPackument>
-      ): void;
-      adduser(
-        uri: string,
-        params: AddUserParams,
-        cb: ClientCallback<AddUserResponse>
-      ): void;
-    };
-  }
+  type Instance = {
+    get(
+      uri: string,
+      params: GetParams,
+      cb: ClientCallback<import("../domain/packument").UnityPackument>
+    ): void;
+    adduser(
+      uri: string,
+      params: AddUserParams,
+      cb: ClientCallback<AddUserResponse>
+    ): void;
+  };
+}
 
-  const RegClient: new (...args: unknown[]) => RegClient.Instance;
+declare module "another-npm-registry-client" {
+  const RegClient: new (this: Instance, config?: { log: unknown }) => Instance;
   export = RegClient;
 }
