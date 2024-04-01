699beec172a942e7982ae1f0447e89cffaa1537b
Ramon Brullo <kingvanti650@gmail.com>
2024-04-01 14:17:38 +0200

    refactor: extract npmrc domain logic (#210)
    * refactor: move file
    
    Move npmrc type to own file.
    
    * refactor: extract function
    
    Extract function for setting token in npmrc

---
+++ src/domain/npmrc.ts
@@ -0,0 +1,48 @@
+import { RegistryUrl } from "./registry-url";
+
+/**
+ * The content lines of a npmrc file.
+ */
+export type Npmrc = ReadonlyArray<string>;
+
+/**
+ * An empty npmrc (No lines).
+ */
+export const emptyNpmrc: Npmrc = [];
+
+/**
+ * Adds or updates the auth-token for a registry.
+ * @param npmrc The current npmrc.
+ * @param registry The registry to set the token for.
+ * @param token The token.
+ */
+export function setToken(
+  npmrc: Npmrc,
+  registry: RegistryUrl,
+  token: string
+): Npmrc {
+  let lines = [...npmrc];
+  const quotes = /[?=]/.test(token) ? '"' : "";
+  // get the registry url without http protocol
+  let registryUrl = registry.slice(registry.search(/:\/\//) + 1);
+  // add trailing slash
+  if (!registryUrl.endsWith("/")) registryUrl = registryUrl + "/";
+  const index = lines.findIndex(function (element, index) {
+    if (element.indexOf(registryUrl + ":_authToken=") !== -1) {
+      // If an entry for the auth token is found, replace it
+      lines[index] = element.replace(
+        /authToken=.*/,
+        "authToken=" + quotes + token + quotes
+      );
+      return true;
+    }
+    return false;
+  });
+  // If no entry for the auth token is found, add one
+  if (index === -1) {
+    lines.push(registryUrl + ":_authToken=" + quotes + token + quotes);
+  }
+  // Remove empty lines
+  lines = lines.filter((l) => l);
+  return lines;
+}
