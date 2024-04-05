acd7b733ca3baa581bd6e8953f6b8561ddc5ddde
Ramon Brullo <kingvanti650@gmail.com>
2024-04-05 11:14:04 +0200

    refactor: child-process logic
    Use result return type

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -1,40 +1,33 @@
 import childProcess from "child_process";
+import { AsyncResult, Err, Ok } from "ts-results-es";
+
+/**
+ * Error that might occur when running a child process.
+ */
+export type ChildProcessError = childProcess.ExecException;
 
 /**
  * @param command A shell command to execute.
  * @param trim Whether to trim whitespace from command output.
- * @returns A promise that resolve to the output of the shell command, or an
- * error.
- * @example const output = await execute("ls -alh");
+ * @returns Result with either stdout string or an error.
  */
 export default function execute(
   command: string,
   { trim }: { trim: boolean }
-): Promise<string> {
-  /**
-   * @param {Function} resolve A function that resolves the promise.
-   * @param {Function} reject A function that fails the promise.
-   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-   */
-  return new Promise(function (resolve, reject) {
-    /**
-     * @param {Error} error An error triggered during the execution of the
-     * "childProcess.exec" command.
-     * @param {string|Buffer} stdout The result of the shell command execution.
-     * @param {string|Buffer} stderr The error resulting of the shell command
-     * execution.
-     * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
-     */
-    childProcess.exec(command, function (error, stdout, stderr) {
-      if (error) {
-        reject();
-        return;
-      }
-      if (stderr) {
-        reject(stderr);
-        return;
-      }
-      resolve(trim ? stdout.trim() : stdout);
-    });
-  });
+): AsyncResult<string, ChildProcessError> {
+  return new AsyncResult(
+    new Promise(function (resolve) {
+      childProcess.exec(command, function (error, stdout, stderr) {
+        if (error) {
+          resolve(Err(error));
+          return;
+        }
+        if (stderr) {
+          resolve(Err(new Error(stderr)));
+          return;
+        }
+        resolve(Ok(trim ? stdout.trim() : stdout));
+      });
+    })
+  );
 }
