de37955e32ef0334fa7effa7821e197a6e6d43ba
Ramon Brullo <kingvanti650@gmail.com>
2024-04-07 16:03:23 +0200

    feat: get builtin packages

---
+++ src/services/builtin-packages.ts
@@ -0,0 +1,61 @@
+import { ReleaseVersion } from "../domain/editor-version";
+import { AsyncResult } from "ts-results-es";
+import {
+  GetEditorInstallPathError,
+  tryGetEditorInstallPath,
+} from "../io/special-paths";
+import { DomainName } from "../domain/domain-name";
+import { CustomError } from "ts-custom-error";
+import path from "path";
+import { NotFoundError, tryGetDirectoriesIn } from "../io/file-io";
+import { IOError } from "../common-errors";
+
+/**
+ * Error for when an editor-version is not installed.
+ */
+export class EditorNotInstalledError extends CustomError {
+  constructor(
+    /**
+     * The version that is not installed.
+     */
+    public readonly version: ReleaseVersion
+  ) {
+    super();
+  }
+}
+
+/**
+ * Errors which may occur when getting the builtin packages for an editor-version.
+ */
+export type BuiltinPackagesError =
+  | GetEditorInstallPathError
+  | EditorNotInstalledError
+  | IOError;
+
+/**
+ * Attempts to get the names of all built-in packages for a Unity editor.
+ * The editor must be installed for this to work.
+ * @param editorVersion The editors version.
+ */
+export function tryGetBuiltinPackagesFor(
+  editorVersion: ReleaseVersion
+): AsyncResult<ReadonlyArray<DomainName>, BuiltinPackagesError> {
+  const pathResult = tryGetEditorInstallPath(editorVersion);
+  if (pathResult.isErr()) return pathResult.toAsyncResult();
+  const installPath = pathResult.value;
+  const packagesDir = path.join(
+    installPath,
+    "Editor/Data/Resources/PackageManager/BuiltInPackages"
+  );
+
+  return (
+    tryGetDirectoriesIn(packagesDir)
+      // We can assume correct format
+      .map((names) => names as DomainName[])
+      .mapErr((error) =>
+        error instanceof NotFoundError
+          ? new EditorNotInstalledError(editorVersion)
+          : error
+      )
+  );
+}
