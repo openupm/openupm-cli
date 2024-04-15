c967275e22931412a4b3b705c9c41f93b92edc6a
Ramon Brullo <kingvanti650@gmail.com>
2024-04-15 18:23:58 +0200

    test: add describe blocks

--- test/project-manifest-io.test.ts
+++ test/project-manifest-io.test.ts
@@ -33,78 +33,95 @@ describe("project-manifest io", () => {
     await mockProject.restore();
   });
 
-  it("should load valid manifest", async () => {
-    const manifestResult = await tryLoadProjectManifest(mockProject.projectPath)
-      .promise;
-
-    expect(manifestResult).toBeOk((manifest) =>
-      expect(manifest).toEqual({ dependencies: {} })
-    );
+  describe("path", () => {
+    it("should determine correct manifest path", () => {
+      const manifestPath = manifestPathFor("test-openupm-cli");
+      const expected = path.join(
+        "test-openupm-cli",
+        "Packages",
+        "manifest.json"
+      );
+      expect(manifestPath).toEqual(expected);
+    });
   });
-  it("should fail when manifest is missing", async () => {
-    const manifestResult = await tryLoadProjectManifest("/invalid-path")
-      .promise;
 
-    expect(manifestResult).toBeError((error) =>
-      expect(error).toBeInstanceOf(NotFoundError)
-    );
-  });
-  it("should fail when manifest has invalid json", async () => {
-    const manifestPath = manifestPathFor(mockProject.projectPath);
-    fse.writeFileSync(manifestPath, "invalid data");
+  describe("load", () => {
+    it("should load valid manifest", async () => {
+      const manifestResult = await tryLoadProjectManifest(
+        mockProject.projectPath
+      ).promise;
 
-    const manifestResult = await tryLoadProjectManifest(mockProject.projectPath)
-      .promise;
-    expect(manifestResult).toBeError((error) =>
-      expect(error).toBeInstanceOf(FileParseError)
-    );
-  });
-  it("should save manifest", async () => {
-    let manifest = (
-      await tryLoadProjectManifest(mockProject.projectPath).promise
-    ).unwrap();
-    expect(manifest).not.toHaveDependencies();
-    manifest = addDependency(
-      manifest,
-      makeDomainName("some-pack"),
-      makeSemanticVersion("1.0.0")
-    );
-    expect(
-      await trySaveProjectManifest(mockProject.projectPath, manifest).promise
-    ).toBeOk();
-    const manifest2 = (
-      await tryLoadProjectManifest(mockProject.projectPath).promise
-    ).unwrap();
-    expect(manifest2).toEqual(manifest);
-  });
-  it("should determine correct manifest path", () => {
-    const manifestPath = manifestPathFor("test-openupm-cli");
-    const expected = path.join("test-openupm-cli", "Packages", "manifest.json");
-    expect(manifestPath).toEqual(expected);
-  });
-  it("should not save scoped-registry with empty scopes", async () => {
-    // Add and then remove a scope to force an empty scoped-registry
-    const testDomain = "test" as DomainName;
-    let initialManifest = buildProjectManifest((manifest) =>
-      manifest.addScope(testDomain)
-    );
-    initialManifest = mapScopedRegistry(
-      initialManifest,
-      exampleRegistryUrl,
-      (registry) => {
-        return removeScope(registry!, testDomain);
-      }
-    );
+      expect(manifestResult).toBeOk((manifest) =>
+        expect(manifest).toEqual({ dependencies: {} })
+      );
+    });
 
-    // Save and load manifest
-    expect(
-      await trySaveProjectManifest(mockProject.projectPath, initialManifest)
-        .promise
-    ).toBeOk();
-    const savedManifest = (
-      await tryLoadProjectManifest(mockProject.projectPath).promise
-    ).unwrap();
+    it("should fail when manifest is missing", async () => {
+      const manifestResult = await tryLoadProjectManifest("/invalid-path")
+        .promise;
 
-    expect(savedManifest.scopedRegistries).toHaveLength(0);
+      expect(manifestResult).toBeError((error) =>
+        expect(error).toBeInstanceOf(NotFoundError)
+      );
+    });
+
+    it("should fail when manifest has invalid json", async () => {
+      const manifestPath = manifestPathFor(mockProject.projectPath);
+      fse.writeFileSync(manifestPath, "invalid data");
+
+      const manifestResult = await tryLoadProjectManifest(
+        mockProject.projectPath
+      ).promise;
+      expect(manifestResult).toBeError((error) =>
+        expect(error).toBeInstanceOf(FileParseError)
+      );
+    });
+  });
+
+  describe("save", () => {
+    it("should save manifest", async () => {
+      let manifest = (
+        await tryLoadProjectManifest(mockProject.projectPath).promise
+      ).unwrap();
+      expect(manifest).not.toHaveDependencies();
+      manifest = addDependency(
+        manifest,
+        makeDomainName("some-pack"),
+        makeSemanticVersion("1.0.0")
+      );
+      expect(
+        await trySaveProjectManifest(mockProject.projectPath, manifest).promise
+      ).toBeOk();
+      const manifest2 = (
+        await tryLoadProjectManifest(mockProject.projectPath).promise
+      ).unwrap();
+      expect(manifest2).toEqual(manifest);
+    });
+
+    it("should not save scoped-registry with empty scopes", async () => {
+      // Add and then remove a scope to force an empty scoped-registry
+      const testDomain = "test" as DomainName;
+      let initialManifest = buildProjectManifest((manifest) =>
+        manifest.addScope(testDomain)
+      );
+      initialManifest = mapScopedRegistry(
+        initialManifest,
+        exampleRegistryUrl,
+        (registry) => {
+          return removeScope(registry!, testDomain);
+        }
+      );
+
+      // Save and load manifest
+      expect(
+        await trySaveProjectManifest(mockProject.projectPath, initialManifest)
+          .promise
+      ).toBeOk();
+      const savedManifest = (
+        await tryLoadProjectManifest(mockProject.projectPath).promise
+      ).unwrap();
+
+      expect(savedManifest.scopedRegistries).toHaveLength(0);
+    });
   });
 });
