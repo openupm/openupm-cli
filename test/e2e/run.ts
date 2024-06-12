9edbc36e683a9a1c084c78f960db6fb228da1ea0
Ramon Brullo <kingvanti650@gmail.com>
2024-06-12 11:59:40 +0200

    log openupm output

--- test/e2e/run.ts
+++ test/e2e/run.ts
@@ -26,8 +26,16 @@ export async function runOpenupm(
   const stdOut = Array.of<string>();
   const stdErr = Array.of<string>();
 
-  openupmProcess.stdout!.on("data", (data) => stdOut.push(data.toString()));
-  openupmProcess.stderr!.on("data", (data) => stdErr.push(data.toString()));
+  openupmProcess.stdout!.on("data", (data) => {
+    const line = data.toString();
+    console.log(line);
+    return stdOut.push(line);
+  });
+  openupmProcess.stderr!.on("data", (data) => {
+    const line = data.toString();
+    console.log(line);
+    return stdErr.push(line);
+  });
 
   return await new Promise<AppOutput>((resolve) =>
     openupmProcess.on("exit", (code) =>
