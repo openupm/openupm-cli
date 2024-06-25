78af478774954868f25a7e8a1d99879f42d96239
Ramon Brullo <kingvanti650@gmail.com>
2024-06-25 08:49:43 +0200

    add jsdoc

--- src/services/unity-package-check.ts
+++ src/services/unity-package-check.ts
@@ -3,13 +3,26 @@ import { AsyncResult } from "ts-results-es";
 import { CheckUrlExists } from "../io/check-url";
 import { GenericNetworkError } from "../io/common-errors";
 
+/**
+ * Error which may occur when checking whether a package is a Unity package.
+ */
 export type CheckIsUnityPackageError = GenericNetworkError;
 
+/**
+ * Function for checking whether a package is an official Unity package.
+ * @param packageName The name of the package.
+ * @returns A boolean indicating whether the package is a Unity package.
+ */
 export type CheckIsUnityPackage = (
   packageName: DomainName
 ) => AsyncResult<boolean, CheckIsUnityPackageError>;
 
-export function makeCheckIsUnityPackage(checkUrlExists: CheckUrlExists): CheckIsUnityPackage {
+/**
+ * Makes a {@link CheckIsUnityPackage} function.
+ */
+export function makeCheckIsUnityPackage(
+  checkUrlExists: CheckUrlExists
+): CheckIsUnityPackage {
   return (packageName) => {
     // A package is an official Unity package if it has a documentation page
     const url = `https://docs.unity3d.com/Manual/${packageName}.html`;
