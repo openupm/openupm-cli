433021f340dfb23c89e5e8516f92624d1e5ff7a0
Ramon Brullo <kingvanti650@gmail.com>
2024-07-03 16:15:37 +0200

    refactor: drop unused code

--- src/cli/error-logging.ts
+++ src/cli/error-logging.ts
@@ -6,109 +6,6 @@ import { SemanticVersion } from "../domain/semantic-version";
 import { EOL } from "node:os";
 import { ResolvePackumentVersionError } from "../packument-version-resolving";
 
-export function suggestCheckingWorkingDirectory(log: Logger) {
-  log.notice("", "Are you in the correct working directory?");
-}
-
-export function notifyManifestMissing(log: Logger, filePath: string) {
-  log.error("", `Could not locate your project manifest at "${filePath}".`);
-  suggestCheckingWorkingDirectory(log);
-}
-
-export function suggestFixErrorsInProjectManifest(log: Logger) {
-  log.notice(
-    "",
-    "Please fix the errors in your project manifest and try again."
-  );
-}
-
-export function notifySyntacticallyMalformedProjectManifest(log: Logger) {
-  log.error("", "Project manifest file contained json syntax errors.");
-  suggestFixErrorsInProjectManifest(log);
-}
-
-export function notifySemanticallyMalformedProjectManifest(log: Logger) {
-  log.error(
-    "",
-    "Project manifest is valid json but was not of the correct shape."
-  );
-  suggestFixErrorsInProjectManifest(log);
-}
-
-export function notifyManifestLoadFailedBecauseIO(log: Logger) {
-  log.error(
-    "",
-    "Could not load project manifest because of a file-system error."
-  );
-}
-
-export function notifyManifestWriteFailed(log: Logger) {
-  log.error(
-    "",
-    "Could not save project manifest because of a file-system error."
-  );
-}
-
-export function notifyNotUsingWsl(log: Logger) {
-  log.error("", "No wsl detected.");
-  log.notice("", "Please make sure you are actually running openupm in wsl.");
-}
-
-export function notifyChildProcessError(log: Logger) {
-  log.error("", "A child process encountered an error.");
-}
-
-export function notifyMissingEnvForUpmConfigPath(
-  log: Logger,
-  variableNames: string[]
-) {
-  const nameList = variableNames.map((name) => `"${name}"`).join(", ");
-  log.error(
-    "",
-    "Could not determine upm-config path because of missing home environment variables."
-  );
-  log.notice(
-    "",
-    `Please make sure that you set one of the following environment variables: ${nameList}.`
-  );
-}
-
-export function notifySyntacticallyMalformedUpmConfig(log: Logger) {
-  log.error("", "Upm-config file contained toml syntax errors.");
-  log.notice("", "Please fix the errors in your upm-config and try again.");
-}
-
-export function notifyProjectVersionMissing(log: Logger, filePath: string) {
-  log.error(
-    "",
-    `Could not locate your projects version file (ProjectVersion.txt) at "${filePath}".`
-  );
-  suggestCheckingWorkingDirectory(log);
-}
-
-export function suggestFixErrorsInProjectVersionFile(log: Logger) {
-  log.notice(
-    "",
-    "Please fix the errors in your project version file and try again."
-  );
-}
-
-export function notifySyntacticallyMalformedProjectVersion(log: Logger) {
-  log.error(
-    "",
-    "Project version file (ProjectVersion.txt) file contained yaml syntax errors."
-  );
-  suggestFixErrorsInProjectVersionFile(log);
-}
-
-export function notifySemanticallyMalformedProjectVersion(log: Logger) {
-  log.error(
-    "",
-    "Project version file (ProjectVersion.txt) file is valid yaml but was not of the correct shape."
-  );
-  suggestFixErrorsInProjectVersionFile(log);
-}
-
 export function notifyPackumentNotFoundInAnyRegistry(
   log: Logger,
   packageName: DomainName
@@ -132,10 +29,6 @@ export function notifyPackumentNotFoundInManifest(
     `The package "${packageName}" was not found in your project manifest.`
   );
   log.notice("", "Please make sure you have spelled the name correctly.");
-}
-
-export function notifyNoVersions(log: Logger, packageName: DomainName) {
-  log.error("", `The package ${packageName} has no versions.`);
 }
 
 export function notifyOfMissingVersion(
