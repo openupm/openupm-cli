91478775b7d04ef84998ecd61ba303253fda3da1
Ramon Brullo <kingvanti650@gmail.com>
2024-08-21 14:50:58 +0200

    make view e2e

---
+++ test/e2e/view.test.ts
@@ -0,0 +1,53 @@
+import { ResultCodes } from "../../src/cli/result-codes";
+import { runOpenupm } from "./run";
+import { prepareHomeDirectory } from "./setup/directories";
+
+describe("view packages", () => {
+  it("should fail if package version was specified", async () => {
+    const homeDir = await prepareHomeDirectory();
+
+    const output = await runOpenupm(homeDir, [
+      "view",
+      "dev.comradevanti.opt-unity@2.0.0",
+    ]);
+
+    expect(output.code).toEqual(ResultCodes.Error);
+    expect(output.stdErr).toEqual(
+      expect.arrayContaining([
+        expect.stringContaining("please do not specify a version"),
+      ])
+    );
+  });
+
+  it("should fail if package is not found", async () => {
+    const homeDir = await prepareHomeDirectory();
+
+    const output = await runOpenupm(homeDir, [
+      "view",
+      "not.existent.package.123456",
+    ]);
+
+    expect(output.code).toEqual(ResultCodes.Error);
+    expect(output.stdErr).toEqual(
+      expect.arrayContaining([expect.stringContaining("could not be found")])
+    );
+  });
+
+  it("should print package information", async () => {
+    const homeDir = await prepareHomeDirectory();
+
+    const output = await runOpenupm(homeDir, [
+      "view",
+      "dev.comradevanti.opt-unity",
+    ]);
+
+    expect(output.code).toEqual(ResultCodes.Ok);
+    expect(output.stdOut).toEqual(
+      expect.arrayContaining([
+        expect.stringContaining(
+          "dev.comradevanti.opt-unity@3.5.0 | Unlicense | versions: 10"
+        ),
+      ])
+    );
+  });
+});
